f0109270f663150d41140cfc5ef77fecd1117ab9
byeong0430 <byeong.kim0430@gmail.com>
2018-07-11 00:42:02 +0000

    established form submission function to update App state

--- src/components/ChatBar.jsx
+++ src/components/ChatBar.jsx
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
+import { addEnterKey } from '../../libs/tweet-functions.js';
 
 export default class ChatBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      newMessage: '',
+      placeholder: 'Type a message and hit ENTER'
+    }
+    this.onMessageChange = this.onMessageChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
   }
-  render() {
-    return (
-      <footer className="chatbar" >
-        <input className="chatbar-username" placeholder={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-      </footer>
+
+  // update newMessate in this.state based on input value
+  onMessageChange(event) {
+    this.setState({
+      newMessage: event.target.value,
+    })
+  }
+  // validate form parameters before sending them back to app.jsx
+  validateForm(event) {
+    event.preventDefault();
+    (event.target.message.value.trim()) && (
+      this.props.sendNewMessage(event.target),
+      // reset current message to ''
+      this.setState({ newMessage: '' })
     );
+  }
+
+  render() {
+    const { newMessage, placeholder } = this.state;
+    return (
+      <footer className="chatbar">
+        <form onSubmit={this.validateForm}>
+          <input
+            type='text'
+            name='username'
+            className="chatbar-username"
+            placeholder={this.props.currentUser}
+          />
+          <input
+            type='text'
+            name='message'
+            className="chatbar-message"
+            placeholder={placeholder}
+            onChange={this.onMessageChange}
+            value={newMessage}
+          />
+          <input type='submit' id='submit-tweet' />
+        </form>
+      </footer >
+    );
+  }
+
+  componentDidMount() {
+    // submit the form when enter key is detected on input[name="message"]
+    addEnterKey('input[name="message"]', 'submit-tweet');
   }
 }
