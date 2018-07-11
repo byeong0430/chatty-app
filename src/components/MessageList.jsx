16e0e7cea8dcb48ef8b804b883cecd3666af1578
byeong0430 <byeong.kim0430@gmail.com>
2018-07-11 00:43:13 +0000

    create a function that loops messages to create react components

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import Message from './Message.jsx'
+import Message from './Message.jsx';
+
 export default class MessageList extends Component {
+  createMessages() {
+    return this.props.messages.map(message =>
+      <Message key={message.id} message={message} />
+    );
+  }
   render() {
     return (
       <main className="messages">
-        <Message />
+        {this.createMessages()}
       </main>
     );
   }
