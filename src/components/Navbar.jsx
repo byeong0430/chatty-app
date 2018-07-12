9fa93feb7f3ad578688c3f79941f9a794c10ae64
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-12 15:10:26 -0700

    added the number of online clients

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">Chatty</a>
-    </nav>
-  );
+export default class Navbar extends Component {
+  render() {
+    console.log(this.props.onlineClientNum);
+    return (
+      <nav className="navbar" >
+        <a href="/" className="navbar-brand">Chatty</a>
+        <span className='client-status'>{`${this.props.onlineClientNum} users online`}</span>
+      </nav>
+    );
+  }
 }
