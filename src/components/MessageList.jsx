6b8cfffd59ed2c9733b4dd61b3f51f4909fd404f
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-12 12:08:51 -0700

    sent alert as props whenever it is available

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -3,9 +3,18 @@ import Message from './Message.jsx';
 
 export default class MessageList extends Component {
   createMessages() {
-    return this.props.messages.map(message =>
-      <Message key={message.id} message={message} />
-    );
+    // filter all notification messages
+    const { messages } = this.props;
+    const alerts = messages.filter(message => message.type === 'incomingNotification');
+
+    return messages.map(message => {
+      // for each incomingMessage, check if there is any notification associated with it
+      // if so, pass the alert to Message.jsx as props
+      if (message.type === 'incomingMessage') {
+        const thisAlert = alerts.filter(alert => alert.referenceTo === message.id);
+        return <Message key={message.id} alert={thisAlert} message={message} />
+      }
+    })
   }
   render() {
     return (
