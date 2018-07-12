9ce00fb98c65fba68e3c03bed844d3df340c0dc1
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-12 12:09:22 -0700

    added alert to appropriate message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Message extends Component {
-  render() {
-    const { username, content } = this.props.message;
+  constructor(props) {
+    super(props);
+    this.displayMsg = this.displayMsg.bind(this);
+  }
+  displayMsg() {
+    const { alert, message: { username, content } } = this.props;
+    const alertMsg = alert.map(alertItem => <div className="message system">{alertItem.content}</div>)
     return (
       <div>
+        {alertMsg}
         <div className="message">
           <span className="message-username">{username}</span>
           <span className="message-content">{content}</span>
         </div>
-        {/* <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div> */}
       </div>
     );
+  }
+  render() {
+    return (<this.displayMsg />);
   }
 }
