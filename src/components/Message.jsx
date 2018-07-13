949b8a972053969dc27029df52333148c07570f3
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-13 16:27:03 -0700

    added handler functions to conditionally render messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,21 +4,29 @@ import React, { Component } from 'react';
 const convertTextToImg = imgUrl => <img src={imgUrl} />;
 // check if text contains image extensions
 const checkIfImg = data => /\.(jpg|png|gif|svg|ico)$/i.test(data);
-
+const handleMessageTypes = (msgType, msgContent) => {
+  return (msgType === 'incomingNotification')
+    ? <div className='col message-content notification'><em>!</em> {msgContent}</div>
+    : <div className='col message-content'><em>></em> {msgContent}</div>;
+};
+const handleUserName = (name, cssStyle) => {
+  return (name)
+    && <div style={cssStyle} className="col message-username">{name}</div>;
+};
 export default class Message extends Component {
   constructor(props) {
     super(props);
   }
   displayMsg = () => {
-    let { currentUser: { name, color }, message: { username, content } } = this.props;
+    let { currentUser: { name, color }, message: { type, username, content } } = this.props;
+    // set username colour
     const style = (name === username) ? { color } : null;
+    // if message contains img extension, convert it to an actual img
     content = checkIfImg(content) ? convertTextToImg(content) : content;
     return (
-      <div>
-        <div className="message">
-          <span style={style} className="message-username">{username}</span>
-          <span className="message-content">{content}</span>
-        </div>
+      <div className="container message">
+        {handleUserName(username, style)}
+        {handleMessageTypes(type, content)}
       </div>
     );
   }
