1976989b422e2b8a4114fd42009ec25d6cbfa094
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-12 22:32:22 -0700

    added functions to handle text with image extensions

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+// convert text url to image
+const convertTextToImg = imgUrl => <img src={imgUrl} />;
+// check if text contains image extensions
+const checkIfImg = data => /\.(jpg|png|gif|svg|ico)$/i.test(data);
+
 export default class Message extends Component {
   constructor(props) {
     super(props);
   }
   displayMsg = () => {
-    const { currentUser: { name, color }, message: { username, content } } = this.props;
+    let { currentUser: { name, color }, message: { username, content } } = this.props;
     const style = (name === username) ? { color } : null;
+    content = checkIfImg(content) ? convertTextToImg(content) : content;
     return (
       <div>
         <div className="message">
