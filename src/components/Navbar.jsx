5cc30c7848f286896c6026317c55fea98176d812
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-13 15:50:54 -0700

    added symantic html tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ export default class Navbar extends Component {
   render() {
     const clientStatus = (this.props.onlineClientNum > 0) && `${this.props.onlineClientNum} users online`;
     return (
-      <nav className="navbar" >
-        <a href="/" className="navbar-brand">Chatty</a>
-        <span className='client-status'>{clientStatus}</span>
-      </nav>
+      <header>
+        <nav className="navbar" >
+          <a href="/" className="navbar-brand">Chatty</a>
+          <span className='client-status'>{clientStatus}</span>
+        </nav>
+      </header>
     );
   }
 }
